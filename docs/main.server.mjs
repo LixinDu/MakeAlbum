c674317b9b93a639bf35582cbb7cc42e2fc38a86
LixinDu <lx.du@outlook.com>
2024-08-25 18:59:00 +0200

    Init

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ON6ADLNC.mjs";import"./chunk-ASPPYMYT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-7RFNCK2X.mjs";import"./chunk-ASPPYMYT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
